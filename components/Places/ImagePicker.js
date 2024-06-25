cd5c304e01abe36c30a15005b0d733e82fba9966
Prince Garg <43511492+PrinceGarg23@users.noreply.github.com>
2024-06-25 16:49:13 +0530

    Camera module completed

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,80 @@
+import { Alert, Button, StyleSheet, Text, View, Image } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+import { Colors } from "../../constants/colors";
+import { useState } from "react";
+import OutlinedButton from "../UI/OutlinedButton";
+
+function ImagePicker() {
+  const [imageUri, setImage] = useState(null);
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Permission Denied",
+        "You need to grant camera permissions to use this app.",
+        [{ text: "Okay" }]
+      );
+      return false;
+    }
+    return true;
+  }
+
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    setImage(image.assets[0].uri);
+  }
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (imageUri) {
+    imagePreview = <Image source={{ uri: imageUri }} style={styles.image} />;
+  }
+
+  return (
+    <View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton onPress={takeImageHandler} icon="camera">
+        Take Image
+      </OutlinedButton>
+    </View>
+  );
+}
+
+export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
---
+++ components/UI/OutlinedButton.js
@@ -0,0 +1,36 @@
+import { Pressable, StyleSheet, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "../../constants/colors";
+
+function OutlinedButton({ onPress, icon, children }) {
+  return (
+    <Pressable onPress={onPress} style={({pressed})=>[styles.button,pressed && styles.pressed]}>
+      <Ionicons style={styles.icon} name={icon} size={18} color={Colors.primary500} />
+      <Text style={styles.text}>{children}</Text>
+    </Pressable>
+  );
+}
+
+export default OutlinedButton;
+
+const styles = StyleSheet.create({
+  button: {
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    margin: 4,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: Colors.primary500,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+  icon: {
+    marginRight: 6,
+  },
+  text: {
+    color: Colors.primary500,
+  },
+});
